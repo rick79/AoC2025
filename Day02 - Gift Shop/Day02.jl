78f77e68f9b4edf965e97733d673bfbcdd8dc907
Christian Rick <christian@rick.se>
2025-12-03 00:18:52 +0100

    Day 2

--- Day02 - Gift Shop/Day02.jl
+++ Day02 - Gift Shop/Day02.jl
@@ -5,13 +5,6 @@ end
 data = read_data("./Day02 - Gift Shop/data.txt")
 
 
-# function isinvalid(id::Int)
-#     d = Int(floor(log10(id))+1)
-#     (d % 2 != 0) && (return false)
-#     v = id ÷ 10^(d ÷ 2)
-#     return ((v * 10^(d ÷ 2)) + v) == id
-# end
-
 function isinvalid(id::Int, n = nothing)
     d = Int(floor(log10(id))+1)
     for i ∈ range(2, isnothing(n) ? d : n)
@@ -23,30 +16,18 @@ function isinvalid(id::Int, n = nothing)
     return false
 end
 
+
 function part_one(data::Vector{Tuple{Int, Int}})
-    res = 0
-    for r ∈ data
-        for i ∈ range(r[1], r[2])
-            if(isinvalid(i, 2))
-                res = res + i
-            end
-        end
-    end     
-    return res   
+    return sum([i for r ∈ data for i ∈ range(r[1], r[2]) if isinvalid(i, 2)])
 end
 res = part_one(data)
+@assert(res == 22062284697)
 println("Part One: $res")
 
+
 function part_two(data::Vector{Tuple{Int, Int}})
-    res = 0
-    for r ∈ data
-        for i ∈ range(r[1], r[2])
-            if(isinvalid(i))
-                res = res + i
-            end
-        end
-    end     
-    return res   
+   return sum([i for r ∈ data for i ∈ range(r[1], r[2]) if isinvalid(i)])
 end
 res = part_two(data)
+@assert(res == 46666175279)
 println("Part Two: $res")
