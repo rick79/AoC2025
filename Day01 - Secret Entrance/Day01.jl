5a9ca66f53b26621619f86c49dc5b7cda10af54d
Christian Rick <christian@rick.se>
2025-12-04 16:06:39 +0100

    Day 1

--- Day01 - Secret Entrance/Day01.jl
+++ Day01 - Secret Entrance/Day01.jl
@@ -6,28 +6,28 @@ data = read_data("./Day01 - Secret Entrance/data.txt")
 
 
 function part_one(rotations::Array{Int}, state::Int)
-    z = 0
+    res = 0
     for r ∈ rotations
         state = mod(state + r, 100)
-        (state == 0) && (z = z + 1)
+        (state == 0) && (res = res + 1)
     end
-    return z
+    @assert(res == 1081)
+    println("Part One: $res")
 end
-res = part_one(data, 50)
-@assert(res == 1081)
-println("Part One: $res")
-
 
 function part_two(rotations::Array{Int}, state::Int)
-    z = 0
+    res = 0
     for r ∈ rotations
         newstate = state + (r % 100)
-        ((state > 0 && newstate < 0) || newstate > 99 || newstate == 0) && (z = z + 1)
+        ((state > 0 && newstate < 0) || newstate > 99 || newstate == 0) && (res = res + 1)
         state = mod(newstate, 100)
-        z = z + abs(r ÷ 100)
+        res = res + abs(r ÷ 100)
     end
-    return z
+    @assert(res == 6689)
+    println("Part Two: $res")
 end
-res = part_two(data, 50)
-@assert(res == 6689)
-println("Part Two: $res")
+
+part_one(data, 50)
+part_two(data, 50)
+
+
